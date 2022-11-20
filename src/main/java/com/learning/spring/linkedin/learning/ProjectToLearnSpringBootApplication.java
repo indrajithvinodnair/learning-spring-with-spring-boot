179fbbc26aadd7ba2699ebeac610e052d8361f47
indrajithvinodnair <indrajithvinodnair@gmail.com>
2022-11-20 05:30:44 +0530

    (initial-commit)

---
+++ src/main/java/com/learning/spring/linkedin/learning/ProjectToLearnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.learning.spring.linkedin.learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectToLearnSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectToLearnSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learning/spring/linkedin/learning/ProjectToLearnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.spring.linkedin.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectToLearnSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
